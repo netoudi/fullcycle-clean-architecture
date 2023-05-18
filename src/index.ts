05ec3b6590324b9cc386789031394ffa54948110
netoudi <netoudi@rocketmail.com>
2023-05-18 14:44:44 -0300

    entidade-vs-orm

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,13 @@ console.log(customer.toString());
 customer.changeName('Jane Joe');
 
 console.log(customer.toString());
+
+// Complexidade de negócio
+// - Domain (coração da applicação)
+// -- Entity
+// --- customer.ts --> contém a regra de negócio
+//
+// Complexidade acidental
+// - Infra (mundo externo)
+// -- Entity/Model
+// --- customer.ts (get, set) --> mapeado para db, json, text...
