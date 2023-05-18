b3f8a115dacac58e0f0448a70f90aeee63d0d1c4
netoudi <netoudi@rocketmail.com>
2023-05-18 14:30:40 -0300

    principio-da-autovalidacao

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -8,13 +8,27 @@ export class Customer {
     this._id = id;
     this._name = name;
     this._address = address;
+    this.validate();
+  }
+
+  validate(): void {
+    if (this._id.length === 0) {
+      throw new Error('Id is required');
+    }
+    if (this._name.length === 0) {
+      throw new Error('Name is required');
+    }
   }
 
   changeName(name: string): void {
     this._name = name;
+    this.validate();
   }
 
   activate(): void {
+    if (this._address.length === 0) {
+      throw new Error('Address is mandatory to activate a customer');
+    }
     this._active = true;
   }
 
