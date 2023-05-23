9f924ff3ff0a9a78d02c7f86db5c632a5350bfec
netoudi <netoudi@rocketmail.com>
2023-05-23 10:49:19 -0300

    definindo-politicas-de-reward

--- src/service/order.service.test.ts
+++ src/service/order.service.test.ts
@@ -1,8 +1,19 @@
+import { Customer } from '../entity/customer';
 import { Order } from '../entity/order';
 import { OrderItem } from '../entity/order_item';
 import { OrderService } from './order.service';
 
 describe('OrderService unit tests', () => {
+  it('should place an order', () => {
+    const customer = new Customer('c1', 'John Doe');
+    const item = new OrderItem('i1', 'Item 1', 'p1', 10, 1);
+
+    const order = OrderService.placeOrder(customer, [item]);
+
+    expect(customer.rewardPoints).toBe(5);
+    expect(order.total()).toBe(10);
+  });
+
   it('should get total of all orders', () => {
     const item1 = new OrderItem('i1', 'Item 1', 'p1', 100, 1);
     const item2 = new OrderItem('i2', 'Item 2', 'p2', 200, 2);
