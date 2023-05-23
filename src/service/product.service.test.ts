675f2b0642804d1262b8decb8ca8b6668218dd08
netoudi <netoudi@rocketmail.com>
2023-05-23 10:18:28 -0300

    definindo-nosso-productservice

---
+++ src/service/product.service.test.ts
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Product } from '../entity/product';
+import { ProductService } from './product.service';
+
+describe('ProductService unit tests', () => {
+  it('should change the prices of all products', () => {
+    const product1 = new Product('p1', 'Product 1', 10);
+    const product2 = new Product('p1', 'Product 1', 20);
+    const products = [product1, product2];
+
+    ProductService.increasePrice(products, 100);
+
+    expect(product1.price).toBe(100);
+    expect(product2.price).toBe(200);
+  });
+});
