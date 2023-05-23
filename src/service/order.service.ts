275abaafedba634728f6065dcda3ff79289ae037
netoudi <netoudi@rocketmail.com>
2023-05-23 10:41:00 -0300

    realizando-calculo-com-orderservice

--- src/service/order.service.ts
+++ src/service/order.service.ts
@@ -1,1 +1,8 @@
-export class OrderService {}
+/* eslint-disable @typescript-eslint/no-extraneous-class */
+import { type Order } from '@app/entity/order';
+
+export class OrderService {
+  static total(orders: Order[]): number {
+    return orders.reduce((acc, order) => acc + order.total(), 0);
+  }
+}
