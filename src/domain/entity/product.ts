53e3f5b04ad7b07aca2ef67eff5b5add01e7f6d0
netoudi <netoudi@rocketmail.com>
2023-05-25 10:41:51 -0300

    implementando-productrepository

--- src/domain/entity/product.ts
+++ src/domain/entity/product.ts
@@ -32,6 +32,10 @@ export class Product {
     this.validate();
   }
 
+  get id(): string {
+    return this._id;
+  }
+
   get name(): string {
     return this._name;
   }
---
+++ src/infrastructure/repository/product.repository.ts
@@ -0,0 +1,45 @@
+import { Product } from '@app/domain/entity/product';
+import { type ProductRepositoryInterface } from '@app/domain/repository/product-repository.interface';
+import { ProductModel } from '@app/infrastructure/db/sequelize/model/product.model';
+
+export class ProductRepository implements ProductRepositoryInterface {
+  async create(entity: Product): Promise<void> {
+    await ProductModel.create({
+      id: entity.id,
+      name: entity.name,
+      price: entity.price,
+    });
+  }
+
+  async update(entity: Product): Promise<void> {
+    await ProductModel.update(
+      {
+        name: entity.name,
+        price: entity.price,
+      },
+      {
+        where: {
+          id: entity.id,
+        },
+      },
+    );
+  }
+
+  async find(id: string): Promise<Product> {
+    const productModel = await ProductModel.findOne({ where: { id } });
+
+    if (productModel === null) {
+      throw new Error('Product not found');
+    }
+
+    return new Product(productModel.id, productModel.name, productModel.price);
+  }
+
+  async findAll(): Promise<Product[]> {
+    const productModels = await ProductModel.findAll();
+
+    return productModels.map((productModel) => {
+      return new Product(productModel.id, productModel.name, productModel.price);
+    });
+  }
+}
