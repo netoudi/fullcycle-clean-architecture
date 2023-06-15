f61abcffa07476ba6e1a5d575ec20bddb3de6a0f
netoudi <netoudi@rocketmail.com>
2023-06-15 13:48:48 -0300

    criando-primeiro-teste-de-unidade

---
+++ src/usecase/customer/find/customer.find.usecase.unit.test.ts
@@ -0,0 +1,42 @@
+import { CustomerFindUseCase } from '@app/usecase/customer/find/customer.find.usecase';
+import { Customer } from '@app/domain/customer/entity/customer';
+import { Address } from '@app/domain/customer/value-object/address';
+
+const customer = new Customer('123', 'John Doe');
+customer.changeAddress(new Address('Street 1', '1', 'Zipcode', 'City'));
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+const MockRepository = () => {
+  return {
+    create: jest.fn(),
+    update: jest.fn(),
+    find: jest.fn().mockReturnValue(Promise.resolve(customer)),
+    findAll: jest.fn(),
+  };
+};
+
+describe('unit test find customer use case', () => {
+  it('should find a customer', async () => {
+    const customerRepository = MockRepository();
+    const usecase = new CustomerFindUseCase(customerRepository);
+
+    const input = {
+      id: '123',
+    };
+
+    const output = {
+      id: '123',
+      name: 'John Doe',
+      address: {
+        street: 'Street 1',
+        number: '1',
+        zipcode: 'Zipcode',
+        city: 'City',
+      },
+    };
+
+    const result = await usecase.execute(input);
+
+    expect(result).toEqual(output);
+  });
+});
