33c77062d1947d49f4da379cfc8e8fdaa900f7bf
netoudi <netoudi@rocketmail.com>
2023-06-15 15:07:04 -0300

    implementando-testes-de-criar-customer-quando-temos-dados-invalidos

--- src/usecase/customer/create/customer.create.usecase.unit.test.ts
+++ src/usecase/customer/create/customer.create.usecase.unit.test.ts
@@ -1,27 +1,28 @@
 import { CustomerCreateUseCase } from '@app/usecase/customer/create/customer.create.usecase';
-import { CustomerFindUseCase } from '@app/usecase/customer/find/customer.find.usecase';
-import { Customer } from '@app/domain/customer/entity/customer';
-import { Address } from '@app/domain/customer/value-object/address';
-
-const customer = new Customer('123', 'John Doe');
-customer.changeAddress(new Address('Street 1', '1', 'Zipcode', 'City'));
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 const MockRepository = () => {
   return {
     create: jest.fn(),
     update: jest.fn(),
-    find: jest.fn().mockReturnValue(Promise.resolve(customer)),
+    find: jest.fn(),
     findAll: jest.fn(),
   };
 };
 
 describe('unit test create customer use case', () => {
-  it('should create a customer', async () => {
-    const customerRepository = MockRepository();
-    const usecase = new CustomerCreateUseCase(customerRepository);
+  let input = {
+    name: 'John Doe',
+    address: {
+      street: 'Street 1',
+      number: '1',
+      zipcode: 'Zipcode',
+      city: 'City',
+    },
+  };
 
-    const input = {
+  beforeEach(() => {
+    input = {
       name: 'John Doe',
       address: {
         street: 'Street 1',
@@ -30,6 +31,11 @@ describe('unit test create customer use case', () => {
         city: 'City',
       },
     };
+  });
+
+  it('should create a customer', async () => {
+    const customerRepository = MockRepository();
+    const usecase = new CustomerCreateUseCase(customerRepository);
 
     const result = await usecase.execute(input);
 
@@ -43,5 +49,80 @@ describe('unit test create customer use case', () => {
         city: 'City',
       },
     });
+  });
+
+  it('should thrown an error when name is missing', async () => {
+    expect.assertions(1);
+
+    const customerRepository = MockRepository();
+    const usecase = new CustomerCreateUseCase(customerRepository);
+
+    input.name = '';
+
+    try {
+      await usecase.execute(input);
+    } catch (error) {
+      expect((error as Error).message).toBe('Name is required');
+    }
+  });
+
+  it('should thrown an error when street is missing', async () => {
+    expect.assertions(1);
+
+    const customerRepository = MockRepository();
+    const usecase = new CustomerCreateUseCase(customerRepository);
+
+    input.address.street = '';
+
+    try {
+      await usecase.execute(input);
+    } catch (error) {
+      expect((error as Error).message).toBe('Street is required');
+    }
+  });
+
+  it('should thrown an error when number is missing', async () => {
+    expect.assertions(1);
+
+    const customerRepository = MockRepository();
+    const usecase = new CustomerCreateUseCase(customerRepository);
+
+    input.address.number = '';
+
+    try {
+      await usecase.execute(input);
+    } catch (error) {
+      expect((error as Error).message).toBe('Number is required');
+    }
+  });
+
+  it('should thrown an error when zipcode is missing', async () => {
+    expect.assertions(1);
+
+    const customerRepository = MockRepository();
+    const usecase = new CustomerCreateUseCase(customerRepository);
+
+    input.address.zipcode = '';
+
+    try {
+      await usecase.execute(input);
+    } catch (error) {
+      expect((error as Error).message).toBe('Zipcode is required');
+    }
+  });
+
+  it('should thrown an error when city is missing', async () => {
+    expect.assertions(1);
+
+    const customerRepository = MockRepository();
+    const usecase = new CustomerCreateUseCase(customerRepository);
+
+    input.address.city = '';
+
+    try {
+      await usecase.execute(input);
+    } catch (error) {
+      expect((error as Error).message).toBe('City is required');
+    }
   });
 });
