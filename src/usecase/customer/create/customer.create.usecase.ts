133d827d0105171e3bbf0dfcba6bd05d887a498e
netoudi <netoudi@rocketmail.com>
2023-06-15 14:33:44 -0300

    criando-usecase-para-criar-customer

--- src/usecase/customer/create/customer.create.usecase.ts
+++ src/usecase/customer/create/customer.create.usecase.ts
@@ -2,7 +2,9 @@ import {
   type CustomerCreateInputDto,
   type CustomerCreateOutputDto,
 } from '@app/usecase/customer/create/customer.create.dto';
+import { CustomerFactory } from '@app/domain/customer/factory/customer.factory';
 import { type CustomerRepositoryInterface } from '@app/domain/customer/repository/customer-repository.interface';
+import { Address } from '@app/domain/customer/value-object/address';
 
 export class CustomerCreateUseCase {
   private readonly customerRepository: CustomerRepositoryInterface;
@@ -12,8 +14,20 @@ export class CustomerCreateUseCase {
   }
 
   async execute(input: CustomerCreateInputDto): Promise<CustomerCreateOutputDto> {
+    const address = new Address(input.address.street, input.address.number, input.address.zipcode, input.address.city);
+    const customer = CustomerFactory.createWithAddress(input.name, address);
+
+    await this.customerRepository.create(customer);
+
     return {
-      //
+      id: customer.id,
+      name: customer.name,
+      address: {
+        street: customer.address.street,
+        number: customer.address.number,
+        zipcode: customer.address.zipcode,
+        city: customer.address.city,
+      },
     };
   }
 }
