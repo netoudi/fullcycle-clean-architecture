97d5fc9de480f7d28063053ccae7a87ceafddac2
netoudi <netoudi@rocketmail.com>
2023-06-15 15:40:55 -0300

    desenvolvendo-usecase-para-alterar-customer

--- src/usecase/customer/update/customer.update.usecase.ts
+++ src/usecase/customer/update/customer.update.usecase.ts
@@ -3,6 +3,7 @@ import {
   type CustomerUpdateOutputDto,
 } from '@app/usecase/customer/update/customer.update.dto';
 import { type CustomerRepositoryInterface } from '@app/domain/customer/repository/customer-repository.interface';
+import { Address } from '@app/domain/customer/value-object/address';
 
 export class CustomerUpdateUseCase {
   private readonly customerRepository: CustomerRepositoryInterface;
@@ -12,8 +13,22 @@ export class CustomerUpdateUseCase {
   }
 
   async execute(input: CustomerUpdateInputDto): Promise<CustomerUpdateOutputDto> {
+    const customer = await this.customerRepository.find(input.id);
+
+    const address = new Address(input.address.street, input.address.number, input.address.zipcode, input.address.city);
+    customer.changeName(input.name);
+    customer.changeAddress(address);
+    await this.customerRepository.update(customer);
+
     return {
-      //
+      id: customer.id,
+      name: customer.name,
+      address: {
+        street: customer.address.street,
+        number: customer.address.number,
+        zipcode: customer.address.zipcode,
+        city: customer.address.city,
+      },
     };
   }
 }
