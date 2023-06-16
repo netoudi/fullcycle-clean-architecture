278da471d9ac0f52c4ce11cdb2a06b77bcb7b209
netoudi <netoudi@rocketmail.com>
2023-06-16 16:16:16 -0300

    implementando-endpoint-de-listagem

--- src/infrastructure/api/routes/customers.route.ts
+++ src/infrastructure/api/routes/customers.route.ts
@@ -1,4 +1,5 @@
 import { CustomerCreateUseCase } from '@app/usecase/customer/create/customer.create.usecase';
+import { CustomerListUseCase } from '@app/usecase/customer/list/customer.list.usecase';
 import express, { type Request, type Response } from 'express';
 import { CustomerRepository } from '@app/infrastructure/customer/repository/sequelize/customer.repository';
 
@@ -19,6 +20,18 @@ customersRoute.post('/', async (req: Request, res: Response) => {
     };
 
     const output = await usecase.execute(input);
+
+    res.send(output);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+customersRoute.get('/', async (req: Request, res: Response) => {
+  const usecase = new CustomerListUseCase(new CustomerRepository());
+
+  try {
+    const output = await usecase.execute({});
 
     res.send(output);
   } catch (error) {
